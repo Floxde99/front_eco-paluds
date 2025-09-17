72b0af6cd66b6fb911ca6f07e7d3e1487a901b9a
Floxde99 <florian.fchr@hotmail.com>
2025-09-17 11:07:32 +0200

    fix footer

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 export function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-200 mt-0">
-      <div className="mx-auto max-w-6xl px-6 py-10 grid grid-cols-1 md:grid-cols-4 gap-8">
+    <footer className="bg-gray-900 text-gray-200 ">
+      <div className="mx-auto px-6 py-10 flex flex gap-8 place-content-between">
         <div className="md:col-span-3 flex items-start gap-3">
           <img src="/logo.jpg" alt="EcoConnect Paluds" className="w-8 h-8 rounded object-cover" />
           <div>
@@ -22,9 +22,9 @@ export function Footer() {
           <Link className="block hover:text-white transition-colors" to="/cgu">CGU</Link>
         </div>
       </div>
-      <div className="border-t border-gray-800">
+      <div className="border-t border-gray-800 text-center">
         <div className="mx-auto max-w-6xl px-6 py-4 text-xs text-gray-400">
-          © 2024 EcoConnect Paluds. Tous droits réservés.
+          © {new Date().getFullYear()} Eco Paluds. Tous droits réservés.
         </div>
       </div>
     </footer>
